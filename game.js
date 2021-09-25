9f597498c9477eba5f747d99176093be60d69a53
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2021-09-25 14:24:54 -0600

    Add info to class files

---
+++ game.js
@@ -0,0 +1,15 @@
+class Game {
+  constructor(ghoul) {
+    this.player = ghoul;
+  }
+  trackGameData() {
+
+  }
+  trackPlayerTurn() {
+
+  }
+  
+};
+
+var vampPlayer = new Player(ghoul);
+var skullPlayer = new Player(ghoul);
